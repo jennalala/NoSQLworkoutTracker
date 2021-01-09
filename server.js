4e98b8c6804766fcf8cf3f60e90ba6ed5834fcd3
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-09 11:39:16 -0500

    added cont for routes in server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
+const Workout = require("./models/workout");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,11 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
+
+const apiRoutes = require("./routes/apiRoutes")(app);
+const indexRoutes = require("./routes/index")(app);
 
 // routes
 app.use(require("./routes"));
