47f3c6acd33552b6ae235d0b2c87fc186072787b
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-09 18:59:38 -0500

    making sure all changes are saved before heroku create

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
+// const path = require("path");
 const Workout = require("./models/workout");
 
 const PORT = process.env.PORT || 3000;
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
