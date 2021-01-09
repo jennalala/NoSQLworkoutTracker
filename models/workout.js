f504e5d95b8f2a51dd0c9e77843a1f9c7c4a9088
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-08 23:12:55 -0500

    workout.js in models to match workout.js in public

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,50 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+    
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
