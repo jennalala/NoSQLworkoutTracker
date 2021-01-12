bee37f7e4f7619d9fa3bc65a6f3a21e28e37a787
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-12 12:55:23 -0500

    got rid of mongoose requirement in api route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const mongoose = require("mongoose");
 
 router.get("/api/workouts", (req, res) => {
   db.Workout.aggregate([
