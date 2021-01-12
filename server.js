10b3c15200a32b54829501d1facc7776289593d5
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-12 13:06:25 -0500

    im just taking stabs to see if moving stuff will fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const apiRoutes= require("./routes/apiRoute");
-const htmlRoutes = require("./routes/htmlRoute");
+const apiRoute = require("./routes/apiRoute");
+const htmlRoute = require("./routes/htmlRoute");
 
 const PORT= process.env.PORT || 8080;
 
@@ -13,13 +13,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Serves Up public Directory
+//public directory
 app.use(express.static("public"));
 
 app.use(apiRoute);
 app.use(htmlRoute);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
 { 
 useNewUrlParser: true,
 useUnifiedTopology: true,
